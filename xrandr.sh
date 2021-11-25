ee6a0758807f8b7474c509137f942a181389e644
chenhm <chenhm@gmail.com>
2021-11-25 11:14:23 +0800

    Create xrandr.sh

---
+++ xrandr.sh
@@ -0,0 +1,4 @@
+cvt 1920 980
+xrandr --newmode "1920x980_60.00"  156.00  1920 2040 2240 2560  980 983 993 1017 -hsync +vsync
+xrandr --addmode Virtual1 "1920x980_60.00"
+xrandr --output Virtual1 --mode "1920x980_60.00"
